518ac38ecde0e352cc2627306897acd445e07981
Ihor <tsake.love@gmail.com>
2020-12-02 11:55:49 +0200

    start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = 8000 || process.env.PORT;
 app.get("/", function(request, response){
 
     response.send("<h2>testAzure</h2>");
